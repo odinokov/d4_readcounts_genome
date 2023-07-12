b23676bf9f7bbdc0fa367251a98ae498f452a7e3
odinokov <12473191+odinokov@users.noreply.github.com>
2023-07-12 14:05:07 +0800

    Update d4_readcounts_genome.sh

--- d4_readcounts_genome.sh
+++ d4_readcounts_genome.sh
@@ -114,7 +114,7 @@ trap cleanup EXIT
 
 # Use GNU parallel to perform the operation on each chromosome
 echo "${chromosomes}" | \
-    parallel --no-notice "d4tools view -A ${D4_FILE} {} | \
+    parallel -j ${CPU} --no-notice "d4tools view -A ${D4_FILE} {} | \
     awk 'NF == 4' |\
     sort --parallel=${CPU} -k 1,1 -k2,2n -u -V | \
     bedtools map -a ${file1Mb} -b - -c 4 -o sum > \
